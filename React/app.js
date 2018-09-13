9607b6a1d4447905bbc3dbcf77bd064ca7aa7398
Vedant Mukherjee <vedantmukherjee@Vedants-MacBook-Pro.local>
2018-09-13 15:29:39 -0400

    Setting this up for React

---
+++ React/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class creatingApp extends Component {
+    render() {
+        {
+            <div className="topnav">
+                <a className="active" href="#home">Home</a>
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+        }
+    }
+}
