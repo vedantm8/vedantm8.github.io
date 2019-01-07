b8c4fac0b700a9178a84f29f8eeea41bdd1aa1ad
Vedant Mukherjee <vedantmukherjee@ironmanslaptop.lan>
2019-01-07 12:52:29 -0500

    simpleJSX

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import jumbotronPhoto from "./images/jumbotronPhoto.jpg"
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="Portfolio">
-      <div className='React'>
-      </div>
-      </div>
-    );
-  }
-}
-
-export default App;
+      <div className="App">
+        <ul>
+          <li><a href="default.asp">Home</a></li>
+          <li><a href="news.asp">News</a></li>
+          <li><a href="contact.asp">Contact</a></li>
+          <li><a href="about.asp">About</a></li>
+        </ul>
+        <div className="startingPage">
+          
+        </div>
+        </div>
+        );
+      }
+    }
+    
+    export default App;
