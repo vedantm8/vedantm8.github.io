9d2ae12c727f7fb22a3553cca52dd3ca19e80440
Vedant Mukherjee <vedantmukherjee@ironmanslaptop.lan>
2019-01-09 13:06:48 -0500

    finishedWithPortfolio

--- src/App.js
+++ src/App.js
@@ -8,45 +8,58 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <ul>
-            <li><a href="#startingPage">Home</a></li>
-            <li><a href="#Portfolio">Portfolio</a></li>
-            <li><a href="contact.asp">Contact</a></li>
-            <li><a href="about.asp">About</a></li>
-          </ul>
-          <div id="startingPage">
-            <img id="openingPicture" src={FrontPage} alt="Front Page"></img>
-            <a id="gitHubButton" alt="GitHub_Button" href="https://github.com/vedantm8">GitHub</a>
-          </div>
-          <div id="Portfolio">
-            <h1 className="Title">Portfolio</h1>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <div id="Boo-Camp-Spot">
-              <img id="BCSPicture" src={BCSPicture} alt="BootCampSpotPicture"></img>
-              <h4 className="Text" id="BCSText">Visit haunted places around the world.</h4>
-              <br></br>
-              <br></br>
-              <br></br>
-              <a id="BCSGitHub" alt="GitHub_Button" href="https://github.com/BooCampSpot/Boo-Camp-Spot">GitHub</a>
-              <br></br>
-              <a id="BCSLive" alt="GitHub_Button" href="https://boocamp-spot.herokuapp.com/">Live</a>
-            </div>
-            <div id="GeoLert">
-              <img id="GeoLerticture" src={GLPicture} alt="GeoLertPicture"></img>
-              <h4 className="Text" id="BCSText">Visit haunted places around the world.</h4>
-              <br></br>
-              <br></br>
-              <br></br>
-              <a id="BCSGitHub" alt="GitHub_Button" href="https://github.com/Undropout/geolerttest">GitHub</a>
-              <br></br>
-              <a id="BCSLive" alt="GitHub_Button" href="https://undropout.github.io/geolerttest/slackisdownMVP/">Live</a>
-            </div>
-          </div>
+        
+      <ul className="NavBar">
+          <li><a href="#startingPage">Home</a></li>
+          <li><a href="#Portfolio">Portfolio</a></li>
+          <li><a href="#Skills">Skills</a></li>
+          <li><a href="about.asp">About</a></li>
+        </ul>
+        
+        <div id="startingPage">
+          <img id="openingPicture" src={FrontPage} alt="Front Page"></img>
+          <a id="gitHubButton" alt="GitHub_Button" href="https://github.com/vedantm8">GitHub</a>
         </div>
+        
+        <div id="Portfolio">
+          <h1 className="Title">Portfolio</h1>
+          <div className="Boxes" id="Boo-Camp-Spot">
+            <img className="PortfolioPicture" src={BCSPicture} alt="BootCampSpotPicture"></img>
+            <h4 className="PortfolioText">Visit haunted places around the world.</h4>
+            <br></br>
+            <br></br>
+            <br></br>
+            <a className="PortfolioGitHub" alt="GitHub_Button" href="https://github.com/BooCampSpot/Boo-Camp-Spot">GitHub</a>
+            <br></br>
+            <a className="PortfolioLive" alt="GitHub_Button" href="https://boocamp-spot.herokuapp.com/">Live</a>
+          </div>
+          <div className="Boxes">
+            <img className="PortfolioPicture" src={GLPicture} alt="GeoLertPicture"></img>
+            <h4 className="PortfolioText">Location based messager</h4>
+            <br></br>
+            <br></br>
+            <br></br>
+            <a className="PortfolioGitHub" alt="GitHub_Button" href="https://github.com/Undropout/geolerttest">GitHub</a>
+            <br></br>
+            <a className="PortfolioLive" alt="GitHub_Button" href="https://undropout.github.io/geolerttest/slackisdownMVP/">Live</a>
+          </div>
+          <div className="Boxes">
+            <img className="PortfolioPicture" src={GLPicture} alt="GeoLertPicture"></img>
+            <h4 className="PortfolioText">A website utilizing BootStrap.</h4>
+            <br></br>
+            <br></br>
+            <br></br>
+            <a className="PortfolioGitHub" alt="GitHub_Button" href="">GitHub</a>
+            <br></br>
+            <a className="PortfolioLive" alt="GitHub_Button" href="">Live</a>
+            <br></br>
+          </div>
+      </div>
+      <br></br>
+      <br></br>
+      <div className="Skills">
+      <h1 className="Title" id="skillTitle">Skills</h1>
+    </div>
       </div>
     );
   }
