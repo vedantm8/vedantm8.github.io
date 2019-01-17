87d548515681abd18e45dc93ac6926777b2abea2
System Administrator <root@IronMansLaptop.local>
2019-01-16 20:08:04 -0500

    Updates

---
+++ precache-manifest.15450587d09468e5de4a5597999982d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "271d0e4d483034fa9051785489c375b4",
+    "url": "/static/media/GeoLert.271d0e4d.png"
+  },
+  {
+    "revision": "f1a42b96365467f24fcfb1579746efd2",
+    "url": "/static/media/BooCampSpot.f1a42b96.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7af84b35565edec91557",
+    "url": "/static/js/main.7af84b35.chunk.js"
+  },
+  {
+    "revision": "71ce1347d16773b69f94",
+    "url": "/static/js/1.71ce1347.chunk.js"
+  },
+  {
+    "revision": "7af84b35565edec91557",
+    "url": "/static/css/main.4d3b71da.chunk.css"
+  },
+  {
+    "revision": "a3d2aa2bf18a97096cede9d1048e70c1",
+    "url": "/index.html"
+  }
+];
